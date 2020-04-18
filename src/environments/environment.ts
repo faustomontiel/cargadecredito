16ee427dac41bc40f0d299ce69d329d2346cca4c
fausto_montiel <fmontiel@ewaysa.com>
2020-04-18 05:16:26 -0300

    Subo icon, splash y apk config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAhqnxGBfz_8lAfzTJMdZK4dD_g7bYbHH0",
+    authDomain: "cargadecredito-6d65b.firebaseapp.com",
+    databaseURL: "https://cargadecredito-6d65b.firebaseio.com",
+    projectId: "cargadecredito-6d65b",
+    storageBucket: "cargadecredito-6d65b.appspot.com",
+    messagingSenderId: "600395932650",
+    appId: "1:600395932650:web:9754ffceefb621ddd70893"
+  }
 };
 
 /*
